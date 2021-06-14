acad914509f69cb3a6ab98f542c51a61b46c97c8
maxjwebber <57260557+maxjwebber@users.noreply.github.com>
2021-06-14 21:34:22 +0000

    completed quiz question/grading.

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,11 +3,11 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let question = "Who was the first woman in space?";
+let correctAnswer = "Sally Ride";
+let candidateAnswer = "";
 let questions;
 let correctAnswers;
 let candidateAnswers;
@@ -15,22 +15,35 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+  candidateName = input.question("What's your name?\n")
+  console.log("Hey, "+candidateName)
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
+  answer = input.question(question)
+  return answer
 
 }
 
-function gradeQuiz(candidateAnswers) {
+function gradeQuiz(candidateAnswer) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 
+ let grade;
+ console.log(candidateAnswer)
+ console.log(correctAnswer)
 
-  let grade;
+  if (candidateAnswer.toUpperCase() == correctAnswer.toUpperCase())
+  {
+    grade = "Correct";
+  }
+  else
+  {
+    grade = "Incorrect";
+  }
   
+
 
   return grade;
 }
@@ -39,8 +52,9 @@ function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
   
-  askQuestion();
-  gradeQuiz(this.candidateAnswers);
+  candidateAnswer = askQuestion();
+  console.log(candidateAnswer)
+  console.log(gradeQuiz(candidateAnswer));
 }
 
 // Don't write any code below this line //
