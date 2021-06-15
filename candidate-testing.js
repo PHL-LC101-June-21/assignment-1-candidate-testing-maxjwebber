9119f50dd4c3c6ea57baba7757fa068858a01414
maxjwebber <57260557+maxjwebber@users.noreply.github.com>
2021-06-15 17:39:23 +0000

    Added line breaks to questions.

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,7 +8,7 @@ let candidateName = "";
 let question = "Who was the first woman in space?";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
-let questions = ['Who was the first woman in space?','True or false: 5 kilometer == 5000 meters?','(5 + 3)/2 * 10 = ?',"Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?",'What is the minimum crew size for the ISS?'];
+let questions = ['Who was the first woman in space?\n','True or false: 5 kilometer == 5000 meters?\n','(5 + 3)/2 * 10 = ?\n',"Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?\n",'What is the minimum crew size for the ISS?\n'];
 let correctAnswers = ['Sally Ride','true','40','Trajectory','3'];
 let candidateAnswers = [];
 
